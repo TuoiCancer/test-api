bdebc5739f95a69282aff0f99c1ca9108836aa1e
Xuan  Tuoi <tuoittx@ikong.vn>
2023-05-11 13:51:49 +0700

    save

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.json('Hello World!');
+  res.send('Hey this is my API running 🥳');
 });
 
 app.listen(8000, () => {
